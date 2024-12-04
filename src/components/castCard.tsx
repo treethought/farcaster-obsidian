7b81086ad3eaa3a2261ddc89bc9de9687661f676
Cam Sweeney <cpsweene@gmail.com>
2024-12-03 19:52:38 -0800

    show cast channel

--- src/components/castCard.tsx
+++ src/components/castCard.tsx
@@ -1,56 +1,71 @@
 import { Cast, CastEmbed, Embed } from "../client/types";
 
 export const CastCard = (
-  props: { cast: Cast | CastEmbed; embed?: boolean },
+	props: { cast: Cast | CastEmbed; embed?: boolean },
 ) => {
-  if (!props.cast) {
-    return null;
-  }
-  let recastClass = "";
-  if (props.embed) {
-    recastClass = "cast-recast-embed";
-  }
-  return (
-    <div className={`cast-card ${recastClass}`}>
-      <div className="cast-header">
-        <img
-          className="avatar"
-          src={props.cast.author?.pfp_url}
-        />
-        <span className="author">{props.cast.author?.display_name}</span>
-      </div>
+	if (!props.cast) {
+		return null;
+	}
+	let recastClass = "";
+	if (props.embed) {
+		recastClass = "cast-recast-embed";
+	}
+	return (
+		<div className={`cast-card ${recastClass}`}>
+			<div className="cast-header">
+				<div className="cast-header-author">
+					<img
+						className="avatar"
+						src={props.cast.author?.pfp_url}
+					/>
+					<span className="author">{props.cast.author?.display_name}</span>
+				</div>
 
-      {props.cast?.text && (
-        <div className="cast-content">
-          <p>{props.cast.text}</p>
-        </div>
-      )}
-      <div className="cast-media">
-        {props.cast?.embeds?.map((embed, i) => (
-          <div
-            className="cast-embed-container"
-            key={props.cast.hash + "embed" + i}
-          >
-            <EmbedRender embed={embed} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+				{props.cast?.channel && (
+					<pre className="cast-header-channel">/{props.cast.channel.name}</pre>
+				)}
+			</div>
+
+			{props.cast?.text && (
+				<div className="cast-content">
+					<p>{props.cast.text}</p>
+				</div>
+			)}
+			<div className="cast-media">
+				{props.cast?.embeds?.map((embed, i) => (
+					<div
+						className="cast-embed-container"
+						key={props.cast.hash + "embed" + i}
+					>
+						<EmbedRender embed={embed} />
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 const EmbedRender = (props: { embed: Embed }) => {
-  if (props.embed?.cast) {
-    return <CastCard cast={props.embed.cast} embed />;
-  }
-  if (props.embed?.metadata?.image) {
-    return (
-      <img
-        className="cast-embed-image"
-        src={props.embed.url}
-      />
-    );
-  }
+	if (props.embed?.cast) {
+		return <CastCard cast={props.embed.cast} embed />;
+	}
+	if (props.embed?.metadata?.image) {
+		return (
+			<img
+				className="cast-embed-image"
+				src={props.embed.url}
+			/>
+		);
+	}
+	if (props.embed?.metadata?.video) {
+		return (
+			<video
+				className="cast-embed-video"
+				src={props.embed.url}
+				controls
+			/>
+		);
+	}
 
-  return null;
+	return null;
 };
